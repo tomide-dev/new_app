8bd9eba90e1900558005a3982e0c0cd17898efd0
tomide <tomideolubanjo@gmail.com>
2026-03-26 15:55:44 -0700

    New deploy

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "updated version"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
